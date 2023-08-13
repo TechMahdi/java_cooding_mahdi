9e26cd3596c88ab5f69ff8fd9d23793dac56fb4a
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-14 00:41:26 +0600

    Create c to f and f to c.java

---
+++ c to f and f to c.java
@@ -0,0 +1,22 @@
+
+package javaapplication2;
+
+import java.util.Scanner;
+
+
+public class JavaApplication2 {
+
+  
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+            double f,c;
+            c=input.nextDouble();
+            f=(1.8)*c +32;
+            System.out.println(f);
+             System.out.println("\n");
+              f=input.nextDouble();  
+            c=(0.555555555)*(f-32);
+                 System.out.println(c);
+    }
+    
+}
