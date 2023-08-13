6e0a7ec18eafbcdfe12c384b5dfedd17f39866ab
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-13 20:49:44 +0600

    Create assignment_anisul.java

---
+++ assignment_anisul.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+       // System.out.println("Hello, World!");
+       int id =101;
+       String title="iphone15";
+       String   price ="1895 euros ";
+       String description ="perfect product with best quality";
+       String category ="phone ";
+       System.out.println("id =  " +id);
+         System.out.println(" title = " +title);
+           System.out.println(" price = " +price);
+         System.out.println(" description = " +description);
+          System.out.println(" category  = " +category );
+       
+    }
+}
