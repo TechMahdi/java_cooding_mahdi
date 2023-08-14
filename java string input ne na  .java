c190241849b839bf44aeabb14850ae3f4a1f0f3e
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-14 20:57:17 +0600

    Create java string input ne na  .java

---
+++ java string input ne na  .java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+        Scanner input = new Scanner(System.in);
+        int i;
+        float f;
+        String  num;
+      
+          i= input.nextInt();
+            input.nextLine();
+        num= input.next();
+      
+       
+        System.out.println("integer is = "+i);
+         System.out.println("string is = "+num);
+        
+    }
+    
+}
