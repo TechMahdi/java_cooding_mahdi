f8881e73c9c9efb4fb21a61c5488d797289b8ce9
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-16 21:59:57 +0600

    Create bitwise oparetor.java

---
+++ bitwise oparetor.java
@@ -0,0 +1,28 @@
+//assignment 8/////
+//bismillah //
+package javaapplication5;
+
+import java.util.Scanner;
+
+
+public class JavaApplication5 {
+
+    
+    public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+     int a=12;
+     int b=13;
+     int c= a>>2;
+     int d= b<<2;
+    System.out.println(c);
+    System.out.println(d);
+           
+        
+            
+       
+     }       
+    
+}
+
+
+
