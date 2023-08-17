744174201b53cd1cf59acbcfe644d54195ef3ea2
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-17 20:53:21 +0600

    Update assignment 12.java

--- assignment 12.java
+++ assignment 12.java
@@ -33,3 +33,51 @@ public class JavaApplication3 {
     }
     
 }
+
+
+
+
+
+//sum of even 
+
+
+
+
+
+package javaapplication3;
+
+import java.util.Scanner;
+
+
+
+
+public class JavaApplication3 {
+
+   
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m,n,sum=0;
+  
+        m = input.nextInt();
+        n = input.nextInt();
+        for (int i = m; i <=n; i++) {
+            if(i%2==1)
+            {
+                continue;
+            }
+            else if(i%2==0)
+            {
+                sum+=i;
+                
+                System.out.print(" "+i);
+            }
+            
+        }
+             
+        System.out.println(" odd: "+sum);
+       
+      
+    }
+    
+}
+
