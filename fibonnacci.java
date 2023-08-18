349de3f7660b29536a8c30e3833761fec736f7d2
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-18 23:32:13 +0600

    Update fibonnacci.java

--- fibonnacci.java
+++ fibonnacci.java
@@ -25,3 +25,33 @@ public class JavaApplication4 {
     
 
 }
+//another approach 
+                            
+package javaapplication4;
+
+import java.util.Scanner;
+
+public class JavaApplication4 {
+
+    
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        int n=input .nextInt();
+        int first=0;
+        int second =1;
+        int fibo=0;
+       //System.out.print(first+" "+second);
+        for(int  i=1;i<=n;i++)
+        {
+            System.out.println(first);
+           fibo=first+second;
+           first=second;
+           second=fibo;
+
+        }
+       
+          // System.out.println(fibo); 
+    }
+    
+
+}
