6db686984f96707f39ecef90e3e9eec5c25560f7
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-09-05 22:41:31 +0600

    Create Stringbuilder.java

---
+++ Stringbuilder.java
@@ -0,0 +1,27 @@
+package javaapplication5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class arraylist {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner (System.in);
+ StringBuilder m = new StringBuilder("ayan ");
+            m.append(123);
+            m.append('r');
+            m.append(56.77);
+            m.append(3f);
+            m.append("adjsasa");
+            m.append(123);
+            System.out.println("the final string is "+m);
+            m.reverse();
+            System.out.println("the bahha"+m);
+            m.delete(0,5);
+            System.out.println(m);
+            
+        
+    }
+}
